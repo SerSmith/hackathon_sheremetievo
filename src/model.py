bce9461a13c74702a4317e7993f1404b03b7683a
Mila601 <mila_601@mail.ru>
2021-10-16 00:25:52 +0300

    Update model.py
    init commit

---
+++ src/model.py
@@ -0,0 +1,39 @@
+import pyomo.environ as pyo
+from pyomo.opt import SolverFactory
+import pandas as pd
+
+class Data():
+    def __init__(self, data_folder: str='../data'):
+        self.data_folder = data_folder
+        self.aircraft_classes_dict = None
+    
+    def get_aircraft_classes():
+        if aircraft_classes_dict is None:
+
+
+
+class OptimizationSolution():
+    def __init__(self):
+        pass
+
+    def check_solution(self):
+        pass
+
+    def get_solution_file(self):
+        pass   
+
+class OptimizeDay:
+    def __init__(self):
+        pass
+
+    def make_model(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    Data
+
+
+
+
